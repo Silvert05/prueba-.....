af16024256533ea6940f5f6418904c6a0a36fe7d
David <cochadavid2005@gmail.com>
2025-10-22 23:12:35 -0500

    prueba

--- tests/test_suma.py
+++ tests/test_suma.py
@@ -6,4 +6,4 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from app import sumar
 
 def test_suma_correcta():
-    assert sumar(2, 2) == 4 # ❌ intencionalmente incorrecto para fallo inicial
+    assert sumar(2, 2) == 5 # ❌ intencionalmente incorrecto para fallo inicial
